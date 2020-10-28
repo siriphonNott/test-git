a52b447012f2f8e6af320dda763537720a39bacf
nottdev <siriphonnot@gmail.com>
2020-10-29 05:52:26 +0700

    Change nickname

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const name = 'Nott' //from master
 const age = 21 //from develop
 const grade = 3.4 //from master
-const nickname = 'attitude' //from develop
+const nickname = 'mama xstream' //from develop
