c19893b131055b6523e062625a08d9ad2af91125
nottdev <siriphonnot@gmail.com>
2020-10-29 06:09:49 +0700

    merge develop

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const name = 'Nott' //from master
 const age = 21 //from develop
 const grade = 3.4 //from master
-const nickname = '12345678' //from develop
+const nickname = 'abcdefg' //from develop
 const api = 'https://acb.xyz/api/v1/'
