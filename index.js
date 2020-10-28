987b6d9f0a52cab93070185abe09185cf87700a5
nottdev <siriphonnot@gmail.com>
2020-10-29 06:00:41 +0700

    Change nickname and add api

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const name = 'Nott' //from master
 const age = 21 //from develop
 const grade = 3.4 //from master
-const nickname = 'mama xstream' //from develop
+const nickname = '12345678' //from develop
+const api = 'https://acb.xyz/api/v1/'
