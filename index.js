a1fb5bcae47c267032c6e4fe5e31e02cc86fa580
nottdev <siriphonnot@gmail.com>
2020-10-29 05:21:48 +0700

    Fix conflict

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 const name = 'Nott' //from master
 const age = 21 //from develop
 const grade = 3.4 //from master
-<<<<<<< HEAD
-const nickname = 'NottDev' //from master
-=======
 const nickname = 'attitude' //from develop
->>>>>>> e9ba1981ba022f05ce4660c3fcce0672b421d0da
